4bac05b44b135bb15b813672be1b653177df3472
Casady-E <ethancasady1@gmail.com>
2025-06-12 17:17:42 -0600

    Learn about using NOT statements with LIKE and IN. Learn about using AND/BETWEEN/OR statements with WHERE to filter more precisely. Included definitions of them and use cases at end.

--- udacity/sql-for-data-analysis/basic-sql/ex-01.sql
+++ udacity/sql-for-data-analysis/basic-sql/ex-01.sql
@@ -88,7 +88,7 @@ WHERE name LIKE '%one%';
 SELECT *
 FROM accounts
 WHERE name LIKE '%s';
-*/
+
 SELECT name, primary_poc, sales_rep_id
 FROM accounts
 WHERE name IN ('Walmart', 'Target', 'Nordstrom');
@@ -96,3 +96,82 @@ WHERE name IN ('Walmart', 'Target', 'Nordstrom');
 SELECT *
 FROM web_events
 WHERE channel IN ('organic', 'adwords');
+
+SELECT name, primary_poc, sales_rep_id
+FROM accounts
+WHERE name NOT IN ('Walmart', 'Target', 'Nordstrom')
+LIMIT 10;
+
+SELECT *
+FROM web_events
+WHERE channel NOT IN ('organic', 'adwords')
+LIMIT 10;
+
+SELECT *
+FROM accounts
+WHERE name NOT LIKE 'C%'
+LIMIT 10;
+
+SELECT *
+FROM accounts
+WHERE name NOT LIKE '%one%'
+LIMIT 10;
+
+SELECT *
+FROM accounts
+WHERE name NOT LIKE '%s'
+LIMIT 10;
+
+SELECT *
+FROM orders
+WHERE standard_qty > 1000 AND poster_qty = 0 AND gloss_qty = 0
+LIMIT 10;
+
+SELECT *
+FROM accounts
+WHERE name NOT LIKE 'C%' AND name NOT LIKE '%s'
+LIMIT 5;
+
+SELECT *
+FROM orders
+WHERE gloss_qty BETWEEN 24 AND 29
+LIMIT 5;
+
+SELECT *
+FROM web_events
+WHERE channel IN ('organic', 'adwords') AND occurred_at BETWEEN '2016-01-01' AND '2017-01-01'
+ORDER BY occurred_at DESC
+LIMIT 10;
+*/
+
+SELECT id
+FROM orders
+WHERE gloss_qty > 4000 OR poster_qty > 4000
+LIMIT 5;
+
+SELECT *
+FROM orders
+WHERE standard_qty = 0 AND (gloss_qty > 1000 OR poster_qty > 1000)
+LIMIT 5;
+
+SELECT *
+FROM accounts
+WHERE (name LIKE 'C%' OR name LIKE'W%') 
+    AND ((primary_poc LIKE '%ana%' OR primary_poc LIKE '%Ana%') 
+    AND primary_poc NOT LIKE '%eana%')
+LIMIT 5;
+
+/*
+Statement	How to Use It	Other Details
+SELECT	SELECT Col1, Col2, ...	--Provide the columns you want
+FROM	FROM Table	--Provide the table where the columns exist
+LIMIT	LIMIT 10	--Limits based number of rows returned
+ORDER BY	ORDER BY Col	--Orders table based on the column. Used with DESC.
+WHERE	WHERE Col > 5	--A conditional statement to filter your results
+LIKE	WHERE Col LIKE '%me%'	--Only pulls rows where column has 'me' within the text
+IN	WHERE Col IN ('Y', 'N')	--A filter for only rows with column of 'Y' or 'N'
+NOT	WHERE Col NOT IN ('Y', 'N')	--NOT is frequently used with LIKE and IN
+AND	WHERE Col1 > 5 AND Col2 < 3	--Filter rows where two or more conditions must be true
+OR	WHERE Col1 > 5 OR Col2 < 3	--Filter rows where at least one condition must be true
+BETWEEN	WHERE Col BETWEEN 3 AND 5	--Often easier syntax than using an AND
+*/
